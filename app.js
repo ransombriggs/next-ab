2783672ea159590ac43d15f451cc16eb503b8b10
Matthew Andrews <matt@mattandre.ws>
2015-01-05 17:47:12 +0000

    Home page

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.get('/__gtg', function(req, res) {
 	res.status(200).send('OK');
 });
 
+app.get('/', function(req, res) {
+	res.redirect(302, 'https://github.com/financial-times/next-ab');
+});
+
 module.exports.listen = app.listen(process.env.PORT, function() {
 	console.log("Listening on port", process.env.PORT);
 });
