03d06624c03d179095cc84e33ac9192045f15586
Matthew Andrews <matt@mattandre.ws>
2015-01-05 17:41:11 +0000

    Redirection endpoint

--- app.js
+++ app.js
@@ -4,7 +4,13 @@
 var express = require('ft-next-express');
 var app = module.exports = express();
 
-
+app.get('/ab', function(req, res) {
+	var destination = 'http://next.ft.com';
+	if (/https?:\/\/next.ft.com/.test(req.query.location)) {
+		destination = req.query.location;
+	}
+	res.redirect(302, destination);
+});
 
 app.get('/__gtg', function(req, res) {
 	res.status(200).send('OK');
