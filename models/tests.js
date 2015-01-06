55c5eb897e5d413453426eaa79c3d721a3984064
Matthew Andrews <matt@mattandre.ws>
2015-01-06 00:23:13 +0000

    It's 2015 now...

--- models/tests.js
+++ models/tests.js
@@ -7,6 +7,6 @@ module.exports = [
 			var decider = options.eRightsId % 10;
 			return decider === 0 ? true : (decider === 1 ? false : undefined);
 		},
-		expires: new Date("Sat Feb 01 2014 00:00:00 GMT+0000 (GMT)")
+		expires: new Date("Sat Feb 01 2015 00:00:00 GMT+0000 (GMT)")
 	}
 ];
