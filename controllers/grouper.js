2ee053de5c51758f631aa486fda27113c62f81c7
Matt Chadburn <matt.chadburn@ft.com>
2015-02-28 22:13:58 +0000

    Seeded random segmentation on eRights

--- controllers/grouper.js
+++ controllers/grouper.js
@@ -1,35 +1,33 @@
 "use strict";
-var tests = require('../models/tests');
+
+var tests		= require('../models/tests')
+var seedrandom	= require('seedrandom');
+
+console.log(tests);
 
 module.exports = function(req, res) {
+	
 	var eRightsId = req.headers['x-ft-user-id'];
+	
 	if (eRightsId) {
-		var expires = new Date(Date.now() + 1000*60*60*24);
-		var groups = tests.map(function(test) {
-				if (test.expires < new Date())  {
-					return;
-				}
 
-				// If the test expires before the default expiry,
-				// reduce the expiry
-				if (test.expires < expires) {
-					expires = test.expires;
-				}
-				switch(test.allocate({ eRightsId: eRightsId })) {
-					case true:
-						return test.flag + ':on';
-					case false:
-						return test.flag + ':off';
-				}
-			}).filter(function(test) {
-				return test !== undefined;
-			});
-		res.cookie('next-ab', groups.join(',') || 'none', { expires: expires });
+		var expires = new Date(Date.now() + 1000*60*60*24); // 1 day
+		
+		var allocation = tests.map(function (test) {
+			var rng = seedrandom(eRightsId + test.flag);
+			var group = (rng() > 0.5) ? 'variant' : 'control';
+			return test.flag + ':' + group;
+		})
+
+
+		res.cookie('next-ab', allocation.join(','), { expires: expires });
 	}
 
 	var destination = 'http://next.ft.com';
+	
 	if (/https?:\/\/next.ft.com/.test(req.query.location)) {
 		destination = req.query.location;
 	}
+
 	res.redirect(302, destination);
 };
