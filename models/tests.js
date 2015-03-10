01365353148d2eaa6eae970fb2cf7655e686b765
Matt Chadburn <matt@commuterjoy.co.uk>
2015-03-10 12:26:07 +0000

    Remove alpha test from the AB testing model

--- models/tests.js
+++ models/tests.js
@@ -4,9 +4,5 @@ module.exports = [
 	{
 		flag: "aa",
 		expires: new Date("Sat Feb 01 2016 00:00:00 GMT+0000 (GMT)")
-	},
-	{
-		flag: "alphasurvey",
-		expires: new Date("Sat Feb 01 2016 00:00:00 GMT+0000 (GMT)")
 	}
 ];
