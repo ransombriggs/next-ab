5fbb8cd9603a7b63fc16c480e99b29633d6c1c8d
Matt Chadburn <matt.chadburn@ft.com>
2015-07-30 12:54:14 +0100

    WIP session api

---
+++ server/middleware/session.js
@@ -0,0 +1,11 @@
+
+
+module.exports = function (res, req, next) { 
+	
+	// FIXME - if session is present in the request header hit the session id to derive UUID, otherwise just called next()
+	return fetch('https://session-next.ft.com/uuid', {
+		headers: req.headers
+	})
+
+}
+
