d52394d1101b91ae795b870c43a2f3b6d824410e
Matt Chadburn <matt.chadburn@ft.com>
2015-08-03 23:18:16 +0100

    device -> id

--- server/middleware/set-allocation-header.js
+++ server/middleware/set-allocation-header.js
@@ -4,10 +4,10 @@
 var allocate = require('../allocate');
 
 module.exports = function(req, res, next) {
-	var allocation = allocate(res.locals.tests, res.locals.device);
+	var allocation = allocate(res.locals.tests, res.locals.id);
 	res.set('x-ft-ab', (allocation) ? allocation : '-');
 	if (allocation) {
-		res.set('ft-device-id', res.locals.device);
+		res.set('ft-device-id', res.locals.id);
 	}
 	next();
 };
--- server/middleware/set-allocation-id.js
+++ server/middleware/set-allocation-id.js
@@ -7,7 +7,7 @@ var getAllocationID = require('../promise-of-id');
 module.exports = function(req, res, next){
 	getAllocationID(req, res)
 		.then(function(id) {
-			res.locals.device = id;
+			res.locals.id = id;
 			next();
 		})
 		.catch(function(e) {
