7315000c73485dd43dc82b6ce006b2606b04cd85
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2015-08-04 10:21:45 +0100

    Removed node-fetch as express comes with a fetch polyfill

--- server/promise-of-id.js
+++ server/promise-of-id.js
@@ -1,14 +1,14 @@
 
 'use strict';
 
-require('node-fetch');
 var nodeUuid	= require('node-uuid');
 
 module.exports = function(req) {
 
 	// Requests receive an A/B allocation for each feature that has an active A/B test.
 	// A/B allocation is based on an allocation ID. Ideally the allocation ID is the
-	// user's uuid, but if the user is anonymous, it fails back to their device ID.
+	// user's uuid, which is detected if given an FT session token.
+	// If the user is anonymous, an allocation ID is generate and returned.
 	// See: http://git.svc.ft.com/projects/NEXT/repos/preflight-requests/browse/src/tasks/session.js
 	var sessionToken = req.get('ft-session-token') || req.get('x-ft-session-token');
 	var allocationId = req.get('ft-allocation-id');
@@ -16,7 +16,7 @@ module.exports = function(req) {
 
 	//console.log(sessionToken, allocationId, isAnonymous);
 
-	// If uuid is not provided, attempt to load it via the session api.
+	// If an ft-session-token is provided, attempt to load uuid via the session api.
 	if (sessionToken) {
 		return fetch('https://session-next.ft.com/uuid', {
 			headers: {
@@ -32,17 +32,15 @@ module.exports = function(req) {
 		.then(function(json) {
 			return json.uuid;
 		});
-
 	}
 
-	// If neither uuid nor session were provided, use the user's device ID.
+	// If an allocation ID is provided, use that.
 	if (allocationId) {
 		return Promise.resolve(allocationId);
 	}
 
-	// If neither uuid, session nor deviceID were provided, but an isAnonymous header
-	// was, what does that mean? In any case, fail to a random generated uuid.
-	// This'll be cached if the CDN caches the allocationID.
+	// If neither ft-session-token nor allocation ID were provided,
+	// use a random-generated uuid.
 	if (isAnonymous) {
 		return Promise.resolve(nodeUuid());
 	}
